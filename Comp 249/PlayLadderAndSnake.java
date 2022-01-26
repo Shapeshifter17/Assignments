3fd293a2845ef06da5ac45c2a4b2ba836eceb860
Andrei Barbulescu <andreibarbulescu17@gmail.com>
2022-01-26 12:24:05 -0500

    Create PlayLadderAndSnake.java

---
+++ Comp 249/PlayLadderAndSnake.java
@@ -0,0 +1,8 @@
+public class PlayLadderAndSnake {
+    public static void main(String[] args) {
+
+        LadderAndSnake game = new LadderAndSnake(2);
+        System.out.println(game.flipDice());
+
+    }
+}
