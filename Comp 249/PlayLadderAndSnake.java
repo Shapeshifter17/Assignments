23e200cf07da7d05c33c310a19c1f32a7ccb6f13
Andrei Barbulescu <andreibarbulescu17@gmail.com>
2022-02-02 10:37:49 -0500

    Update PlayLadderAndSnake.java

--- Comp 249/PlayLadderAndSnake.java
+++ Comp 249/PlayLadderAndSnake.java
@@ -1,43 +1,21 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
 public class PlayLadderAndSnake {
     public static void main(String[] args) {
-
-     //   LadderAndSnake game = new LadderAndSnake(4);
-     //   System.out.println(game.flipDice());
-     //   System.out.println(Arrays.toString(game.playerOrder()));
         Scanner scan = new Scanner(System.in);
-        System.out.println("How many players are there? (between 2 and 4)");
-
-        int players = 0;
-        while(true){
+        for(int attempt = 0; attempt < 4; attempt++ )
+        {
+            System.out.println("Please Enter Numbers of Players (between 2and 4): ");
             int userIn = scan.nextInt();
-
             if (userIn >= 2 && userIn <=4){
-                players = userIn;
-
+                LadderAndSnake g1 = new LadderAndSnake(userIn);
+                g1.play();
                 break;
             }
-            else
-                System.out.println("How many players are there? (between 2 and 4)");
-
+            else {
+                System.out.println("Invlaid!");
+            }
         }
-        int[] playerArr = new int[players-1];
-        System.out.println(playerArr.length);
-
-
-        Gamers gamer1 = new Gamers();
-        Gamers gamer2 = new Gamers();
-        Gamers gamer3 = new Gamers();
-        Gamers gamer4 = new Gamers();
-        int num1 = gamer1.flipDice();
-        int num2 = gamer2.flipDice();
-        int num3 = gamer3.flipDice();
-        int num4 = gamer4.flipDice();
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(num3);
-        System.out.println(num4);
+        System.out.println("Game is terminated");
+        scan.close();
     }
 }
